5d2dd218c35282d3852bfcf730ce9c0346ab71f7
Shubham Kumar <kshubhambgs123@gmail.com>
2026-03-22 18:03:56 +0530

    Add Nakama migrations to start.sh script

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 # Fix potential postgresql:// vs postgres:// protocol issue
 ADAPTER_URL=$(echo "$NAKAMA_DATABASE_URL" | sed 's/^postgresql/postgres/')
 
+echo "Running Nakama migrations..."
+/nakama/nakama migrate up --database.address "$ADAPTER_URL"
+
 echo "Starting Nakama with transformed Database URL..."
 
 exec /nakama/nakama run \
